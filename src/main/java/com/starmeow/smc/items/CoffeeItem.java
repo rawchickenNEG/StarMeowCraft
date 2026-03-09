89ac60ee203b6cff58d1e4891c313bae703bb4f4
rawchickenNEG <1589900935@qq.com>
2026-03-09 09:11:20 -0400

    coffee item model, change lang

--- src/main/java/com/starmeow/smc/items/CoffeeItem.java
+++ src/main/java/com/starmeow/smc/items/CoffeeItem.java
@@ -2,14 +2,25 @@ package com.starmeow.smc.items;
 
 import com.starmeow.smc.config.Config;
 import com.starmeow.smc.init.SoundRegistry;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.stats.Stat;
+import net.minecraft.stats.Stats;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class CoffeeItem extends Item {
     public CoffeeItem(Properties p_41383_) {
@@ -20,7 +31,9 @@ public class CoffeeItem extends Item {
     public ItemStack finishUsingItem(ItemStack stack, Level world, LivingEntity entity) {
         if (isEdible()) {
             entity.eat(world, stack.copy());
-            if(entity instanceof Player player){
+            if(entity instanceof ServerPlayer player){
+                Stat<?> stat = Stats.CUSTOM.get(Stats.TIME_SINCE_REST);
+                player.getStats().setValue(player, stat, 0);
                 player.getCooldowns().addCooldown(stack.getItem(), Config.COFFEE_COOLDOWN.get());
             }
         }
@@ -33,5 +46,13 @@ public class CoffeeItem extends Item {
 
     public SoundEvent getEatingSound() {
         return SoundEvents.GENERIC_DRINK;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn)
+    {
+        String string = "tooltip.smc." + stack.getItem();
+        tooltip.add(Component.translatable(string).withStyle(ChatFormatting.DARK_AQUA));
+        tooltip.add(Component.translatable(string + "_1").withStyle(ChatFormatting.DARK_AQUA));
     }
 }
