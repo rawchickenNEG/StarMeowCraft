72ce515a644ab8ae7df07e8fef6156ae37566eb2
rawchickenNEG <1589900935@qq.com>
2026-04-04 23:05:55 -0400

    ancient smithing table

--- src/main/java/com/starmeow/smc/blocks/AncientSmithingTable.java
+++ src/main/java/com/starmeow/smc/blocks/AncientSmithingTable.java
@@ -10,11 +10,16 @@ import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.inventory.SmithingMenu;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.CraftingTableBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.phys.BlockHitResult;
+
+import java.util.Collections;
+import java.util.List;
 
 public class AncientSmithingTable extends CraftingTableBlock {
     private static final Component CONTAINER_TITLE = Component.translatable("container.smc.upgrade");
@@ -37,5 +42,10 @@ public class AncientSmithingTable extends CraftingTableBlock {
             p_56431_.awardStat(Stats.INTERACT_WITH_SMITHING_TABLE);
             return InteractionResult.CONSUME;
         }
+    }
+
+    @Override
+    public List<ItemStack> getDrops(BlockState state, LootParams.Builder builder) {
+        return Collections.singletonList(new ItemStack(this, 1));
     }
 }
