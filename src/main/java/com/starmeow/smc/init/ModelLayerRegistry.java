c0e55b9d0ab9c935387bbc818cc9284e56fe98ed
rawchickenNEG <1589900935@qq.com>
2026-04-04 23:08:15 -0400

    Chicken harvester

--- src/main/java/com/starmeow/smc/init/ModelLayerRegistry.java
+++ src/main/java/com/starmeow/smc/init/ModelLayerRegistry.java
@@ -1,6 +1,8 @@
 package com.starmeow.smc.init;
 
+import com.starmeow.smc.client.model.ChickenHarvesterModel;
 import com.starmeow.smc.client.model.SpearModel;
+import com.starmeow.smc.client.model.SwordAuraModel;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,5 +14,8 @@ public class ModelLayerRegistry {
     @SubscribeEvent
     public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(SpearModel.LAYER_LOCATION, SpearModel::createBodyLayer);
+        event.registerLayerDefinition(ChickenHarvesterModel.LAYER_LOCATION, ChickenHarvesterModel::createBodyLayer);
+        event.registerLayerDefinition(SwordAuraModel.LAYER_LOCATION, SwordAuraModel::createBodyLayer);
+
     }
 }
