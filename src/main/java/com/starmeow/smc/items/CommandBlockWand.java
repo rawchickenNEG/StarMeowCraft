af14d543288b72b1e90660cbcb1c50ac5c6d3656
rawchickenNEG <1589900935@qq.com>
2026-04-09 16:03:49 -0400

    Command wand

--- src/main/java/com/starmeow/smc/items/CommandBlockWand.java
+++ src/main/java/com/starmeow/smc/items/CommandBlockWand.java
@@ -2,27 +2,38 @@ package com.starmeow.smc.items;
 
 import com.starmeow.smc.entities.projectiles.MagicArrow;
 import com.starmeow.smc.entities.projectiles.RainbowArrow;
+import com.starmeow.smc.helper.ItemHelper;
 import com.starmeow.smc.init.ItemRegistry;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.*;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BrushableBlock;
+import net.minecraft.world.level.block.entity.BrushableBlockEntity;
+import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
@@ -68,8 +79,32 @@ public class CommandBlockWand extends Item {
     @Override
     public InteractionResultHolder<ItemStack> use(Level p_40672_, Player p_40673_, InteractionHand p_40674_) {
         ItemStack itemstack = p_40673_.getItemInHand(p_40674_);
-        p_40673_.startUsingItem(p_40674_);
+        if(p_40673_.isCrouching() && p_40673_.getAbilities().instabuild){
+            return InteractionResultHolder.pass(itemstack);
+        }else{
+            p_40673_.startUsingItem(p_40674_);
+        }
         return InteractionResultHolder.success(itemstack);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext p_41341_) {
+        Level level = p_41341_.getLevel();
+        BlockPos blockpos = p_41341_.getClickedPos();
+        ItemStack itemstack = p_41341_.getItemInHand();
+        String loottableRL = itemstack.getHoverName().getString();
+        Player player = p_41341_.getPlayer();
+        if(player != null && player.isCrouching() && player.getAbilities().instabuild){
+            if(level.getBlockEntity(blockpos) instanceof BrushableBlockEntity brushable){
+                brushable.setLootTable(new ResourceLocation(loottableRL), 0);
+            }
+            if(level.getBlockEntity(blockpos) instanceof RandomizableContainerBlockEntity chestEntity){
+                chestEntity.setLootTable(new ResourceLocation(loottableRL), 0);
+            }
+            level.playSound((Player)null, player.getX(), player.getY(), player.getZ(), SoundEvents.ENCHANTMENT_TABLE_USE, SoundSource.PLAYERS, 0.8F, 2.0F);
+            return InteractionResult.sidedSuccess(level.isClientSide);
+        }
+        return InteractionResult.PASS;
     }
 
     @Override
@@ -92,11 +127,17 @@ public class CommandBlockWand extends Item {
         return UseAnim.BOW;
     }
 
+    @Override
+    public Component getName(ItemStack stack) {
+        return ItemHelper.rainbowColor(super.getName(stack), 20, true);
+    }
+
+
     @OnlyIn(Dist.CLIENT)
     public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn)
     {
         String string = "tooltip.smc." + stack.getItem();
         tooltip.add(Component.translatable(string).withStyle(ChatFormatting.BLUE));
-        tooltip.add(Component.translatable(string + "_1").withStyle(ChatFormatting.AQUA));
+        tooltip.add(Component.translatable(string + "_1").withStyle(ChatFormatting.DARK_PURPLE));
     }
 }
